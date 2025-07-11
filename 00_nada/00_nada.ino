4bfb754bb679f7be5ab2e2f93826e488b999016f
AMONSULTARI <amonsul_tari@hotmail.com>
2025-07-11 13:31:34 +0200

    11-07

--- 00_nada/00_nada.ino
+++ 00_nada/00_nada.ino
@@ -1,9 +1,32 @@
-void setup() {
-  // put your setup code here, to run once:
+#define PIN_LED 2
+#define PIN_ROJO 12
 
+int contador = 0;
+
+void setup() {
+  Serial. begin(115200);
+  delay(50);
+  pinMode(PIN_LED, OUTPUT);
+  pinMode(PIN_ROJO, OUTPUT);
+  Serial.println("Encendido.");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+  digitalWrite(PIN_LED, HIGH);
+  digitalWrite(PIN_ROJO, LOW);
+  Serial.println("Encendido.");
 
+  contador ++;
+
+  Serial.println(contador);
+
+  delay(500);
+
+
+  digitalWrite(PIN_LED, LOW);
+  digitalWrite(PIN_ROJO, HIGH);
+
+  Serial.println("Apagado.");
+  delay(500);
 }
